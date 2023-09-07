137aebb282ad5f773d915b5300705fdb1f841d2e
yujinni <86652449+yujin37@users.noreply.github.com>
2023-09-07 11:04:48 +0900

    Model updated | 실행 포트번호 수정

--- Model/app.py
+++ Model/app.py
@@ -297,4 +297,4 @@ def get_score():
       return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
