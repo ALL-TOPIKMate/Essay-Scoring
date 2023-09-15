5d4258a8fcf31136357274c8c620500825618d32
yujinni <86652449+yujin37@users.noreply.github.com>
2023-09-15 17:25:04 +0900

    GPT File updated | 환경변수 명령어 및 일부 설정 변경

--- Model/gpt_response.py
+++ Model/gpt_response.py
@@ -2,16 +2,16 @@ import os
 import openai
 
 def gpt_response(question, quest_content, user_answer):
-    openai.api_key = os.environ.get('OPENAI_API_KEY') #호출할 때는 메모장에서 가져오기
+    openai.api_key = os.getenv('OPENAI_API_KEY') #호출할 때는 메모장에서 가져오기
     user_content = "질문: " + question +"\n" + '제시문: ' + quest_content + "\n\n" + '사용자 답: ' + user_answer
     message_info = [{
         "role": "system",
-        "content": "질문과 제시문이 주어지면 사용자 답안이 이에 따라 주제, 문제점, 결론이 개요에 맞게 잘 구성되었는지 채점하여 잘한 점과 부족한 점에 대해 정리해주는 한국어 교사. 글자 수에 대한 지적은 하지 않는다. 점수는 50점 만점으로 진행한다."
+        "content": "질문과 제시문이 주어지면 사용자 답안이 이에 따라 주제, 문제점, 결론이 잘 구성되었는지 채점하여 잘한 점과 부족한 점에 대해 정리해주는 한국어 교사. 글자 수에 대한 지적은 하지 않는다."
         }]
     message_info.append({"role":"user","content":user_content})
     message_info.append({
         "role": "assistant",
-        "content": "지시사항:\n점수: 00점\n잘한점:\n부족한점:"
+        "content": "지시사항:\n잘한점:\n부족한점:"
         })
     
     response = openai.ChatCompletion.create(
