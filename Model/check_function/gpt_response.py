3ef3168e102a835df0a416ec06737f7fafac31da
yujinni <86652449+yujin37@users.noreply.github.com>
2023-09-22 13:59:11 +0900

    GPT File updated | 지시 사항 및 일부 수정

--- Model/check_function/gpt_response.py
+++ Model/check_function/gpt_response.py
@@ -1,27 +1,27 @@
 import os
 import openai
 
-def gpt_response(question, quest_content, user_answer):
+def gpt_response(question, quest_content, user_answer, answer):
     openai.api_key = os.getenv('OPENAI_API_KEY') #호출할 때는 메모장에서 가져오기
-    user_content = "질문: " + question +"\n" + '제시문: ' + quest_content + "\n\n" + '사용자 답: ' + user_answer
+    user_content = "문제: " + question +"\n" + '제시문: ' + quest_content + "\n\n" + '사용자 답안: ' + user_answer + '예시 답안: ' + answer
     message_info = [{
         "role": "system",
-        "content": "질문과 제시문이 주어지면 사용자 답안이 이에 따라 주제, 문제점, 결론이 잘 구성되었는지 채점하여 잘한 점과 부족한 점에 대해 정리해주는 한국어 교사. 글자 수에 대한 지적은 하지 않는다."
+        "content": "질문과 제시문이 주어지면 예시답안과 비교하여 잘한 점과 부족한 점에 대해 채점해주는 한국어 교사."
         }]
     message_info.append({"role":"user","content":user_content})
     message_info.append({
         "role": "assistant",
-        "content": "지시사항:\n잘한점:\n부족한점:"
+        "content": "잘한점:\n부족한점:"
         })
     
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages = message_info,
-        temperature=0.7,
-        max_tokens=512,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+    model="gpt-3.5-turbo",
+    messages= message_info,
+    temperature=1,
+    max_tokens=512,
+    top_p=0.7,
+    frequency_penalty=0,
+    presence_penalty=0
     )
     answer = response['choices'][0]['message']['content']
     return answer
