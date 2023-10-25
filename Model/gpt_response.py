2b35710309f288bd614096898a9769fc5146b4db
yujinni <86652449+yujin37@users.noreply.github.com>
2023-10-25 20:16:20 +0900

    refacter: GPT File updated | GPT 전송 시스템 메시지 수정

--- Model/gpt_response.py
+++ Model/gpt_response.py
@@ -8,7 +8,7 @@ def gpt_response(question, quest_content, user_answer, answer, length):
     user_content = "문제: " + updated_question +"\n" + '제시문: ' + quest_content + "\n\n" + '사용자 답안: ' + user_answer +"\n"+ '예시 답안' + answer
     message_info = [{
         "role": "system",
-        "content": "문제와 제시문이 주어지면 사용자 답안을 예시 답안과 비교하여 잘한 점과 부족한 점에 대해 채점해주는 한국어 교사. 만점은 45점 기준이다.  글자 수 지적은 하지 않는다.  답변 형식은 JSON 형식으로 제공한다.  Score, Good_Points, Weak_Points로 구성된다."
+        "content": "사용자 답안이 제시문을 바탕으로 충족해 작성했는지 예시 답안과 비교해 채점해주는 한국어 교사. 잘한 점(Good Points), 부족한 점(Weak Points)을 피드백해준다. 45점이 최고점으로 사용자 답안이 제시문 조건에 맞추어 설명이 90%이상 작성되었으면 40~45점, 60%이상~90%미만은 30점~40점, 40%이상~60%미만은 20~30점, 20%이상~40%미만은 10~20점, 10%미만은 10점 아래로 부여한다. 글자 수 지적은 하지 않는다. 답변 형식은 JSON 형식으로 제공한다.  Score, Good Points, Weak Points로 구성된다."
         }]
     message_info.append({"role":"user","content":user_content})
     
